a5fbdb9dc652f40c6ad8495f5cbaec43e768f8f4
Ronald Chan <the@gr8.ninja>
2022-01-08 20:22:20 +0800

    set default configs

--- svelte.config.js
+++ svelte.config.js
@@ -6,9 +6,17 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
 	kit: {
 		adapter: adapter(),
+
+		// disable ssr, because we're using this as a JAMStack
+		ssr: false,
+
+		// enable router for all pages to prevent need to whole page refresh
+		router: true,
+
+		// enable hydrate in dev mode by default so hmr can work everywhere
+		hydrate: process.env.NODE_ENV === 'development', 
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
